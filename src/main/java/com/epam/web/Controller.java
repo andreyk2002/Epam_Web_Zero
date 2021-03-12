e7e764d287a0a0361c5c90c582dc8b5d6c701c53
Andrey <Kuksa>
2021-03-12 17:53:33 +0300

    First project via java servlets

---
+++ src/main/java/com/epam/web/Controller.java
@@ -0,0 +1,32 @@
+package com.epam.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Controller extends HttpServlet {
+
+
+    public Controller() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process(req, resp);
+    }
+
+    private void process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String name  = request.getParameter("Name");
+        // response.getWriter().println("<html><h2>Hello," + name + "</h2></html>");
+
+        request.getRequestDispatcher("init.jsp").forward(request,response);
+    }
+}
